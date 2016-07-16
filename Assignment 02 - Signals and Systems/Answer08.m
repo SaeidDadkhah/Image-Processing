d03562cef3bb36602571b70a198c93fd53368caf
Saeid Dadkhah <Saeid Dadkhah>
2016-07-16 21:55:02 +0430

    assignment 2 is completed with score 100

---
+++ Assignment 02 - Signals and Systems/Answer08.m
@@ -0,0 +1,27 @@
+clear
+
+% Read image
+i = imread('Lena.gif');
+
+% Convert uint8 to double
+d = im2double(i);
+
+% Create low-pass filter
+h = fspecial('average', [5, 5]);
+
+% Apply filter
+filtered = imfilter(d, h, 'replicate');
+
+% Plot original and filtered image
+%{
+subplot(1, 2, 1);
+subimage(d);
+subplot(1, 2, 2);
+subimage(filtered);
+%}
+
+mse = sum(sum((d - filtered).^2)) / (size(d, 1) * size(d, 2));
+display(mse);
+
+psnr = 10 * log10(1^2 / mse);
+display(psnr);
